19fa9a2090f281ff6343dc053042f12a77ed67cc
proNeel02 <prashantdabe02@gmail.com>
2023-06-07 12:52:06 +0530

    Hello

--- server.js
+++ server.js
@@ -1,43 +1,73 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const fs = require("fs");
 
 // Middleware to parse JSON data
 app.use(express.json());
 
+const fileContent = fs.readFileSync("users.json", "utf8");
+const USERS = JSON.parse(fileContent);
 
-const USERS = [];
-
-app.post("/login", (req, res) => {
-
-  
-  res.send("Login!!");
-});
+const questionsFileContent = fs.readFileSync("questions.json", "utf8");
+const QUESTIONS = JSON.parse(questionsFileContent);
 
 app.post("/signup", (req, res) => {
+  // Add Logic to Decode Body
+  // body should have email and password
+  // Store email and password in USERS array (only if the user with same email not exist)
+  const userObj = req.body;
 
-   // Add Logic to Decode Body
-// body should have email and password
- // Store email and password in USERS array (only if the user with same email not exist)
-
-    const userObj =  req.body;
-     
-   if(USERS.find((obj) => obj.email === userObj.email)){
-    res.status(409).json({ error: 'Email already registered' });    
-   }else{
-     USERS.push(userObj);
-    res.status(201).json({ message: 'User signup successful' });
-   }
+  if (USERS.find((obj) => obj.email === userObj.email)) {
+    res.status(409).json({ error: "Email already registered" });
+  } else {
+    USERS.push(userObj);
+    fs.writeFileSync("users.json", JSON.stringify(USERS));
+    res.status(201).json({ message: "User Signup successful" });
+  }
 });
 
+app.post("/login", (req, res) => {
+  // Add Logic to Decode Body
+  // body should have email and password
+  // check user is present inside an USERS array with same email
+  // and password
+
+  // if exist send status code 200 to the client
+  // also send back a token
+
+  // if password is not same return back to status code 401 (incorrect password)
+  // if email not found return 404 ("User Not Found");
+
+  const currObj = req.body;
+
+  USERS.find((obj) => obj.email === currObj.email)
+    ? USERS.find((obj) => obj.password === currObj.password)
+      ? res.status(200).json({ message: "User Found!!" })
+      : res.status(401).json({ error: "Incorrect Password!" })
+    : res.status(404).json({ error: "User Not Found!" });
+});
 app.get("/questions", (req, res) => {
-  res.send("Questions");
+  if (QUESTIONS.length === 0) {
+    res.status(204).json({ message: "No Questions" });
+  }
+  res.status(200).json(QUESTIONS);
+});
+
+app.post("/addQuestion", (req, res) => {
+  const currObj = req.body;
+  if (QUESTIONS.find((obj) => obj.question.title === currObj.title)) {
+    res.status(409).json({ error: "Question Already Exist" });
+  } else {
+    QUESTIONS.push({ question: currObj, id: QUESTIONS.length });
+    fs.writeFileSync("questions.json", JSON.stringify(QUESTIONS));
+    res.status(201).json({ message: "Questions is Created!!" });
+  }
 });
 
 app.get("/submissions", (req, res) => {
-    res.send("subs");
-  });
-  
+  res.send("subs");
+});
 
 app.listen(port, () => {
   console.log(`Server is Running at ${port}`);
